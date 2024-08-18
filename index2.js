dd1784c3f22f8eb53623c9b3e43b9b8cb7f00885
Pradeep7766 <pradeepingle7766@gmail.com>
2024-08-19 04:26:31 +0530

    commit

--- index2.js
+++ index2.js
@@ -1,13 +1,24 @@
-document.getElementById('searchBar').addEventListener('input', function () {
-    let filter = this.value.toUpperCase();
-    let notes = document.getElementsByClassName('note-card');
+// document.getElementById('searchBar').addEventListener('keyup', function () {
+//     let filter = this.value.toUpperCase();
+//     let notesSection = document.getElementById('notesSection');
+//     let notes = document.querySelectorAll('.note-card');
+//     let found = false;
 
-    for (let i = 0; i < notes.length; i++) {
-        let title = notes[i].getElementsByTagName('h2')[0];
-        if (title.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            notes[i].style.display = "";
-        } else {
-            notes[i].style.display = "none";
-        }
-    }
-});
+//     notes.forEach(function (note) {
+//         let title = note.querySelector('h2').textContent || note.querySelector('h2').innerText;
+//         if (title.toUpperCase().indexOf(filter) > -1) {
+//             // Move the matched note to the top
+//             notesSection.prepend(note);
+//             note.style.display = "";
+//             found = true;
+//         } else {
+//             // Hide non-matching notes
+//             note.style.display = "none";
+//         }
+//     });
+
+//     // If no match is found, display a message or handle it accordingly
+//     if (!found) {
+//         console.log('No matching notes found.');
+//     }
+// });
